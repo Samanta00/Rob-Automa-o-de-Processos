96f0696641ae02281056b07317bbdb4fb0ca3116
Ellen Samanta <ellen.samanta@outlook.com>
2024-05-10 18:25:31 -0300

    feat: adicionando a biblioteca requests_html para analisar se url foi bem sucedida

--- pesquisaAutomatizada.py
+++ pesquisaAutomatizada.py
@@ -1,8 +1,26 @@
 from selenium import webdriver
+from requests_html import HTMLSession
 
-driver = webdriver.Chrome()
-driver.maximize_window()
-driver.get("https://www.tce.sp.gov.br/jurisprudencia/")
-print("Application title is", driver.title)
-print("Application URL is", driver.current_url)
-driver.quit()
+
+session = HTMLSession()
+response = session.get("https://www.tce.sp.gov.br/jurisprudencia/")
+
+
+if response.status_code == 200:
+    title = response.html.find("title", first=True).text
+    url = response.url
+
+    print("Application title is", title)
+    print("Application URL is", url)
+
+    driver = webdriver.Chrome()
+    driver.maximize_window()
+    driver.get("https://www.tce.sp.gov.br/jurisprudencia/")
+    print("Application title is", driver.title)
+    print("Application URL is", driver.current_url)
+else:
+    print("Failed to fetch the webpage")
+
+
+
+
