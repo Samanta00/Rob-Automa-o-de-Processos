068c7e1c308a6ebef60a47eaee23676ca102764d
Ellen Samanta <ellen.samanta@outlook.com>
2024-05-11 13:02:18 -0300

    feat: adicionando funcionalidades para serem testadas pelo excel

--- tce/tce/spiders/tce.py
+++ tce/tce/spiders/tce.py
@@ -1,24 +1,30 @@
 import scrapy
-
+import pandas as pd
 
 class TceSpider(scrapy.Spider):
     name = "tce"
-    allowed_domains = ["tce.com"]
+    allowed_domains = ["tce.sp.gov.br"]
     start_urls = ["https://www.tce.sp.gov.br/jurisprudencia/pesquisar?txtTdPalvs=fraude+em+escolas&txtExp=&txtQqUma=&txtNenhPalvs=&txtNumIni=&txtNumFim=&tipoBuscaTxt=Documento&_tipoBuscaTxt=on&quantTrechos=1&processo=&exercicio=2023%2C2022&dataAutuacaoInicio=&dataAutuacaoFim=&dataPubInicio=&dataPubFim=&_relator=1&_auditor=1&_materia=1&_tipoDocumento=1&acao=Executa"]
 
     def parse(self, response):
-        for doc in response.css('.small:nth-child(1) a::text').getall():
-            yield{
-                "doc" : doc,
-                "Nprocesso" : response.css('.small+ .small a::text').getall(),
-                "dataAtuacao" : response.css('.small:nth-child(3) ::text').getall(),
-                "partes" : response.css('.small:nth-child(4) ::text').getall(),
-                "materia" : response.css('.small:nth-child(6) ::text').getall(),
-            }
-        
-
+        dados = []
     
+        for index, doc in enumerate(response.css('.small:nth-child(1) a')):
+            doc_text = doc.css('::text').get().strip()
+            url = doc.css('::attr(href)').get()
+            n_processo = response.css('.small+ .small a ::text')[index].get()
+            data_atuacao = response.css('.small:nth-child(3) ::text')[index].getall()
+            partes = response.css('.small:nth-child(4) ::text')[index].getall() + response.css('.small:nth-child(5) ::text')[index].getall()
+            materia = response.css('.small:nth-child(6) ::text')[index].getall()
 
+            dados.append({
+                "doc": doc_text,
+                "Nprocesso": n_processo,
+                "dataAtuacao": data_atuacao,
+                "partes": partes,
+                "materia": materia,
+                "url": url,
+            })
 
-
-        pass
+        df = pd.DataFrame(dados)
+        df.to_excel('dados_tce.xlsx', index=False)
