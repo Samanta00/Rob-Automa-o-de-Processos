de343ddff386d2269acb8cdd05f353a474c4322c
Ellen Samanta <ellen.samanta@outlook.com>
2024-05-11 17:06:31 -0300

    feat: adicionando conexão ao banco de dados no projeto

--- tce/tce/pipelines.py
+++ tce/tce/pipelines.py
@@ -6,8 +6,53 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
+import mysql.connector
+from mysql.connector import Error
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+MYSQL_HOST = os.getenv("MYSQL_HOST")
+MYSQL_DB = os.getenv("MYSQL_DB")
+MYSQL_USER = os.getenv("MYSQL_USER")
+MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
 
 
 class TcePipeline:
     def process_item(self, item, spider):
+        return item
+
+
+class savingToMysqlPipeline(object):
+
+    def __init__(self):
+        self.create_connection()
+
+    def create_connection(self):
+        try:
+            connection = mysql.connector.connect(
+                host=os.getenv("MYSQL_HOST"),
+                database=os.getenv("MYSQL_DB"),
+                user=os.getenv("MYSQL_USER"),
+                password=os.getenv("MYSQL_PASSWORD")
+            )
+            if connection.is_connected():
+                db_Info = connection.get_server_info()
+                print("Connected to MySQL Server version ", db_Info)
+                cursor = connection.cursor()
+                cursor.execute("select database();")
+                record = cursor.fetchone()
+                print("You're connected to database: ", record)
+
+        except Error as e:
+            print("Error while connecting to MySQL", e)
+        finally:
+            if connection.is_connected():
+                cursor.close()
+                connection.close()
+                print("MySQL connection is closed")
+
+    def process_item(self, item, spider):
+        self.create_connection()
         return item
